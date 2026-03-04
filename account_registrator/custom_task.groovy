a0af5962ae1030845916f16aec3f890ac1c1997b
c00lh0zing <106633420+c00lh0zing@users.noreply.github.com>
2026-03-04 18:19:29 +0300

    Update custom_task.groovy

---
+++ account_registrator/custom_task.groovy
@@ -0,0 +1,8 @@
+                sshagent(['TYZ_SSH']) {
+                    sh """
+                        ssh -o StrictHostKeyChecking=no -l ${SSH_USER} ${DEST_HOST} sudo -u $LOCAL_USER chmod -R 777 /opt/provisioning/$PRJ_NAME/env
+                        scp -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null ./$PRJ_NAME/custom_files/fio_exceptions.txt ${SSH_USER}@${DEST_HOST}:/opt/provisioning/$PRJ_NAME/env/
+                        ssh -o StrictHostKeyChecking=no -l ${SSH_USER} ${DEST_HOST} sudo -u $LOCAL_USER sudo /usr/bin/chown -R $LOCAL_USER:$LOCAL_USER /opt/provisioning
+                        ssh -o StrictHostKeyChecking=no -l ${SSH_USER} ${DEST_HOST} sudo -u $LOCAL_USER chmod 755 -R /opt/provisioning/
+                    """
+                }
